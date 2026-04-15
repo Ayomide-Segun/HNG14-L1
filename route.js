b11c8c9ebedd9c312c11a144278cb6b8fc77b67b
Ayomide-Segun <treasure.segun@gmail.com>
2026-04-15 15:33:05 +0100

    removed test route

--- route.js
+++ route.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { addProfile } = require("./controller");
 
-router.get("/", (req, res) => {
-    console.log("ROOT HIT");
-    res.send("API WORKING");
-});
 router.post('/profiles', addProfile);
 
 module.exports = router;
