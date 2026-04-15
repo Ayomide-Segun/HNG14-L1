33a727c0e2c62d34fe87c9104bbc7947e38753c9
Ayomide-Segun <treasure.segun@gmail.com>
2026-04-15 14:12:24 +0100

    added test route

--- route.js
+++ route.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addProfile } = require("./controller")
+const { addProfile } = require("./controller");
 
+router.get("/", (req, res) => {
+    console.log("ROOT HIT");
+    res.send("API WORKING");
+});
 router.post('/profiles', addProfile);
 
 module.exports = router;
